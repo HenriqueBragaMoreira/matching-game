ec06daff31c27e70d8dd115f703b80096d194e7e
HenriqueBraga <shenrique40moreira@gmail.com>
2023-08-22 23:49:52 -0300

    style: adding styles on page

--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -1,7 +1,8 @@
+import './style.css'
 function CardGame () {
     return `
-        <article>
-            CardGame - OI
+        <article class="card-game">
+            <img src="src/images/alura-pixel.svg" alt="Logo da Alura">
         </article>
     `
 }
